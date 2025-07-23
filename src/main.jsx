c54f915bd338ed5e3135826959efd53dc0efe80d
Gpanazio <panazio@gmail.com>
2025-07-23 13:33:07 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// ✅ Correto: código fora do JSX
 import { getCurrentBoardData as globalGetCurrentBoardData } from "./utils/getCurrentBoardData";
 window.getCurrentBoardData = globalGetCurrentBoardData;
-)
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
