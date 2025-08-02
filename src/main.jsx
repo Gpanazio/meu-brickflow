46927ac85140e53e8d8785ac4eb5de4538f3a119
Gpanazio <panazio@gmail.com>
2025-08-02 19:08:38 -0300

    Use LegacyApp in main entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import LegacyApp from './LegacyApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LegacyApp />
   </StrictMode>,
 )
