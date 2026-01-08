03cc1a56ae1b2ca879a452b66af888e501b60575
Gpanazio <panazio@gmail.com>
2026-01-08 13:33:31 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx' // Mudado de LegacyApp para App
+import App from './App.jsx'
+import './index.css' // Certifique-se que o CSS global é importado se houver
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
