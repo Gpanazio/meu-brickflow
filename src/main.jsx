ebff9cc4b69ab882643f123903012678e1a5ecd6
Gpanazio <panazio@gmail.com>
2026-01-08 13:26:52 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import LegacyApp from './LegacyApp.jsx'
+import App from './App.jsx' // Mudado de LegacyApp para App
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LegacyApp />
+    <App />
   </StrictMode>,
 )
