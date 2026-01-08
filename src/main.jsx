10c9b33d556980d2a745065671aeb3112d254aeb
Gpanazio <panazio@gmail.com>
2026-01-08 13:49:38 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './App.css' // Corrigido: Importa App.css onde estão os estilos globais
+import './App.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
