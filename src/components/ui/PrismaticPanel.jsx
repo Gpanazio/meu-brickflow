9fee83ccd98c186b015a70e2e75f3fe260952b4c
Gpanazio <panazio@gmail.com>
2026-01-12 19:48:20 -0300

    Create PrismaticPanel.jsx

---
+++ src/components/ui/PrismaticPanel.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+
+export default function PrismaticPanel({ children, className, hoverEffect = false, onClick, ...props }) {
+  return (
+    <div 
+      onClick={onClick}
+      className={cn(
+        "glass-panel group transition-all duration-300 ease-out",
+        hoverEffect && "cursor-pointer hover:border-white/20 hover:-translate-y-1",
+        className
+      )}
+      {...props}
+    >
+      {/* Brilho de varredura no Hover */}
+      {hoverEffect && (
+        <div className="absolute inset-0 bg-gradient-to-tr from-transparent via-white/5 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 pointer-events-none z-0" />
+      )}
+      
+      {/* Textura de Ruído */}
+      <div className="absolute inset-0 bg-noise z-0" />
+
+      {/* Conteúdo */}
+      <div className="relative z-10 w-full h-full">
+        {children}
+      </div>
+    </div>
+  );
+}
